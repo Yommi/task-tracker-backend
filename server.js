08be995779bfb22a3ad26e550b3ea3033bd531ec
Yomi <123415710+Yommi@users.noreply.github.com>
2024-12-30 01:14:30 -0500

    changed default port to 3000 for heruko

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ mongoose
   .then(() => console.log('Database Connected ✅'))
   .catch((err) => console.log('Database connection error 🚫', err));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
