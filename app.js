b208aff8d52b3a85f1413086480c5a97b3e165a1
Yomi <123415710+Yommi@users.noreply.github.com>
2024-12-30 02:06:02 -0500

    removed cors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
+// const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const adminRouter = require('./routes/adminRoutes');
 const authRouter = require('./routes/authRoutes');
@@ -18,12 +18,12 @@ if (process.env.NODE_ENV === 'development') {
 // BODY PARSER
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  }),
-);
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     credentials: true,
+//   }),
+// );
 
 app.use(cookieParser());
 
